29021da1ae5ad4016c6c1522e071744bfad410db
Jo HyunA <teniuscom@gmail.com>
2024-03-13 15:34:01 +0900

    랜덤 수 생성

--- util.cpp
+++ util.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-// 3자리 숫자를 입력받는 함수
-string getThreeNumbers(){
-    string s;
-    while(true){
-        cin>>s;
-        if(s.length()==3) break;
-        else cout<<"please enter 3 numbers\n";
+// 3자리 랜덤 수 생성
+string getRandomThreeNumbers(){
+    srand(time(nullptr));
+    int num=0;
+    for(int i=0;i<3;i++){
+        int randNum=rand()%10;
+        num=(num*10)+randNum;
     }
-    return s;
+    return to_string(num);
 }
